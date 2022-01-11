ea8234fcaf395d512660b80e492e10cd5fc1cd6b
503 <503@academicos.cesde.edu.co>
2022-01-11 11:35:03 -0500

    actualizo pagina

---
+++ controlador.js
@@ -0,0 +1,14 @@
+//comentario de linea
+
+/*comentario de bloque, se utiliza para hacer pruebas*/
+
+// definamos variables (ENTRADAS)
+let nombre="jomar"
+let edad=31
+let hinchadelmejor=false
+let estatura=1.60
+
+// SALIDAS
+console.log(nombre)
+
+
