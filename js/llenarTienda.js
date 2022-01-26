b6c0ad89eed0265ce1ce1f51fd5dd50e2eb4df6a
jomar2030 <jomar2030@hotmail.com>
2022-01-25 20:52:48 -0500

    se crea un nuevo espacio

--- js/llenarTienda.js
+++ js/llenarTienda.js
@@ -4,16 +4,16 @@ export function llenarTienda(){
 
     let productos=[
 
-        {nombre:"comic spiderman",precio:1000000,foto:"../img/1.jpg",descripcion:"comics iconico spiderman"},
-        {nombre:"funko spiderman",precio:120000,foto:"../img/2.jpg",descripcion:"funko spiderman"},
-        {nombre:"comic amazing spiderman",precio:100000,foto:"../img/3.jpg",descripcion:"comic amazing spiderman"},
-        {nombre:"juego spierman ps4",precio:150000,foto:"../img/4.jpg",descripcion:"juego ps4 con miles morales"},
-        {nombre:"juego spiderman ps4 1era edicion",precio:80000,foto:"../img/5.jpg",descripcion:"edicion 1 spiderman ps4"},
-        {nombre:"gafas estilo tony stark",precio:500000,foto:"../img/6.jpg",descripcion:"gafas estilo tony stark"},
-        {nombre:"lego spiderman",precio:130000,foto:"../img/7.jpg",descripcion:"lego"},
-        {nombre:"set de funkos",precio:160000,foto:"../img/8.jpg",descripcion:"funkos spiderman"},
-        {nombre:"comic spiderman",precio:115000,foto:"../img/9.jpg",descripcion:"comic amazing #699 spiderman"},
-        {nombre:"comic spiderman",precio:110000,foto:"../img/10.jpg",descripcion:"comics amazing #800spiderman"}
+        {nombre:"comic spiderman",precio:201000,foto:"../img/1.jpg",descripcion:"edicion 1"},
+        {nombre:"funko",precio:62000,foto:"../img/2.jpg",descripcion:"spiderman"},
+        {nombre:"comic spiderman",precio:17000,foto:"../img/3.jpg",descripcion:"la era del traje negro"},
+        {nombre:"juego spierman ps5",precio:319000,foto:"../img/4.jpg",descripcion:"juego ps4 con miles morales"},
+        {nombre:"juego spiderman ps4 1era edicion",precio:95000,foto:"../img/5.jpg",descripcion:"edicion 1 spiderman ps4"},
+        {nombre:"gafas estilo tony stark",precio:587000,foto:"../img/6.jpg",descripcion:"gafas estilo tony stark"},
+        {nombre:"comic  spiderman",precio:130000,foto:"../img/7.jpg",descripcion:"ley y el orden"},
+        {nombre:"comic spiderman",precio:160000,foto:"../img/8.jpg",descripcion:"funkos spiderman"},
+        {nombre:"comic spiderman",precio:115000,foto:"../img/9.jpg",descripcion:"la identidad del duende"},
+        {nombre:"comic spiderman",precio:110000,foto:"../img/10.jpg",descripcion:"la muerte de gwen staicy"}
     ]
 
     console.log(productos)
