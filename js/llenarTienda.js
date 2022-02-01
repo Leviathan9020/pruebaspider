c77a39954e8329dd68bc2d6d31153f4c66b3f13b
jomar2030 <jomar2030@hotmail.com>
2022-01-31 23:11:54 -0500

    se agrego carrito de mercado

--- js/llenarTienda.js
+++ js/llenarTienda.js
@@ -4,16 +4,16 @@ export function llenarTienda(){
 
     let productos=[
 
-        {nombre:"comic spiderman",precio:201000,foto:"../img/1.jpg",descripcion:"edicion 1"},
+        {nombre:"Comic Spiderman",precio:201000,foto:"../img/1.jpg",descripcion:"edicion 1"},
         {nombre:"funko",precio:62000,foto:"../img/2.jpg",descripcion:"spiderman"},
-        {nombre:"comic spiderman",precio:17000,foto:"../img/3.png",descripcion:"la era del traje negro"},
+        {nombre:"La era del Traje Negro",precio:17000,foto:"../img/3.png",descripcion:"spiderman traje negro"},
         {nombre:"juego spierman ps5",precio:319000,foto:"../img/4.jpg",descripcion:"juego ps4 con miles morales"},
         {nombre:"juego spiderman ps4 1era edicion",precio:95000,foto:"../img/5.jpg",descripcion:"edicion 1 spiderman ps4"},
-        {nombre:"gafas estilo tony stark",precio:587000,foto:"../img/6.jpg",descripcion:"gafas estilo tony stark"},
-        {nombre:"comic  spiderman",precio:130000,foto:"../img/7.jpg",descripcion:"ley y el orden"},
-        {nombre:"comic spiderman",precio:160000,foto:"../img/8.jpg",descripcion:"funkos spiderman"},
-        {nombre:"comic spiderman",precio:115000,foto:"../img/9.jpg",descripcion:"la identidad del duende"},
-        {nombre:"comic spiderman",precio:110000,foto:"../img/10.jpg",descripcion:"la muerte de gwen staicy"}
+        {nombre:"Gafas estilo tony stark",precio:587000,foto:"../img/6.jpg",descripcion:"gafas estilo tony stark"},
+        {nombre:"Ley y Orden",precio:130000,foto:"../img/7.jpg",descripcion:"de spiderman ley y el orden"},
+        {nombre:"La Identidad del Duende",precio:160000,foto:"../img/8.jpg",descripcion:"funkos spiderman"},
+        {nombre:"El Regreso del Ladron",precio:115000,foto:"../img/9.jpg",descripcion:"spiderman la identidad del duende"},
+        {nombre:"La Muerte de Gwen Stacy",precio:110000,foto:"../img/10.jpg",descripcion:"la muerte de gwen staicy"}
     ]
 
     //Rutina para recorrer el arreglo y crear las tarjetas de producto 
